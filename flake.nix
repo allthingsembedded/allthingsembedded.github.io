8d9fee8f244209de826515b8107c0a92c686ff5e
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-02-24 23:01:28 +0100

    Add flake to build site

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Flake to build a static site with hugo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    even-theme = {
+      url = "github:olOwOlo/hugo-theme-even/master";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      even-theme,
+      nixpkgs,
+      ...
+    }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forEachSystem =
+        fn:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          fn pkgs
+        );
+    in
+    {
+      packages = forEachSystem (pkgs: rec {
+        allthingsembedded = pkgs.stdenvNoCC.mkDerivation {
+          pname = "allthingsembedded";
+          version = "1.0.0";
+
+          src = self;
+
+          nativeBuildInputs = [
+            pkgs.hugo
+          ];
+
+          buildPhase = ''
+            mkdir -p themes/even/
+            cp -r ${even-theme} themes/even
+            hugo
+          '';
+
+          installPhase = ''
+            cp -r public $out
+          '';
+        };
+
+        default = allthingsembedded;
+      });
+
+      devShells = forEachSystem (pkgs: rec {
+        allthingsembedded = pkgs.mkShellNoCC {
+          nativeBuildInputs = [
+            pkgs.hugo
+          ];
+        };
+
+        default = allthingsembedded;
+      });
+    };
+}
